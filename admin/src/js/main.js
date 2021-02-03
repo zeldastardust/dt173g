f4080e83677e08e2d452b9725cc6794bdc0c4131
Marie Lindström <zeldastardust@hotmail.com>
2021-02-03 23:40:42 +0100

    added sites

--- admin/src/js/main.js
+++ admin/src/js/main.js
@@ -3,6 +3,7 @@
 //variables
 let workEl = document.getElementById("work");
 let studyEl = document.getElementById("study");
+let sitesEl = document.getElementById("sites");
 
 let addWorkbtn= document.getElementById("addWork");
 let companyInput= document.getElementById("company"); 
@@ -16,11 +17,19 @@ let coursenameInput=document.getElementById("coursename");
 let starteduInput=document.getElementById("startedu");
 let stopeduInput=document.getElementById("stopedu");
 
+let addSitesbtn=document.getElementById("addSites");
+let webnameInput=document.getElementById("webname");
+let urlInput=document.getElementById("url");
+let descriptionInput=document.getElementById("description");
+
 //eventlistener
 window.addEventListener('load', getWork);
 window.addEventListener('load', getStudy);
+window.addEventListener('load', getSites);
 addWorkbtn.addEventListener('click',addWork);
 addStudybtn.addEventListener('click', addStudy);
+addSitesbtn.addEventListener('click', addSites);
+
 
 //functions
 function getWork(){
@@ -133,6 +142,63 @@ function addStudy(){
     .then(response=>response.json())
     .then(data=>{
         getStudy();
+    })
+    .catch(error =>{
+        console.log("Error:", error);
+    })
+}
+
+function getSites(){
+    sitesEl.innerHTML='';
+    fetch("http://localhost/dt173g/api/sites.php")
+    .then(response => response.json())
+    .then(data => {
+        data.records.forEach(sites =>{
+            sitesEl.innerHTML +=
+            `<div class="sites">
+            <p>
+            <b>Webbplats:</b> ${sites.webname}
+            </p>
+            <p>
+            <b>url:</b> ${sites.url}
+            </p>
+            <p>
+            <b>Beskrivning:</b> ${sites.description}
+            </p>
+            <button id="${sites.id}" onClick="deleteSite(${sites.id})">Radera</button>
+            </div>`
+           
+        })
+    })
+}
+
+function deleteSite(id){
+    fetch('http://localhost/dt173g/api/sites.php?id='+id, {
+        method:'DELETE',
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        getSites();
+    })
+    .catch(error =>{
+        console.log("Error:", error);
+    })
+}
+
+function addSites(){
+    let webname = webnameInput.value;
+    let url = urlInput.value;
+    let description = descriptionInput.value;
+    
+
+    let sites = {'webname':webname, 'url':url, 'description':description};
+    fetch('http://localhost/dt173g/api/sites.php', {
+        method:'POST',
+        body:JSON.stringify(sites),
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        getSites();
     })
     .catch(error =>{
         console.log("Error:", error);
